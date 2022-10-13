a8846a8b53404dc19b95738aeaa02e090be3f8a3
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-10-13 11:20:48 -0300

    loads settings

--- app/internal/settings.py
+++ app/internal/settings.py
@@ -4,3 +4,8 @@ import os
 class Settings:
     clusterId = os.getenv("CLUSTER_ID")
     scheduler = os.getenv("SCHEDULER", "SGE")
+
+    @classmethod
+    def read_environments(cls):
+        cls.clusterId = os.getenv("CLUSTER_ID")
+        cls.scheduler = os.getenv("SCHEDULER")
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from dotenv import load_dotenv
+import os
+import pathlib
 from fastapi import FastAPI
 from app.routers import jobs
+from app.internal.settings import Settings
+
+BASEDIR = pathlib.Path().resolve()
+os.environ["APP_INSTALLDIR"] = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(
+    pathlib.Path(os.getenv("APP_INSTALLDIR")).joinpath(".env"),
+    override=True,
+)
+Settings.read_environments()
 
 app = FastAPI()
 
 app.include_router(jobs.router)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
