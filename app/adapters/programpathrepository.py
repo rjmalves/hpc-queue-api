1bc983abfaae262afd773d3cbb8f076df97c1f7c
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-10-14 14:42:07 -0300

    fix types

--- app/adapters/programpathrepository.py
+++ app/adapters/programpathrepository.py
@@ -65,7 +65,7 @@ class PEMAWSProgramPathRepository(ABC):
                     installationDirectory=str(versionPath),
                     isManaged=True,
                     args=args,
-                    executablePath=versionPath.joinpath(execFiles[0]),
+                    executablePath=str(versionPath.joinpath(execFiles[0])),
                 )
             )
         return programs
