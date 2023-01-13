50a52eccbb75e723aaa996ef861729eef28f274f
Rogerio <rogerioalves.ee@gmail.com>
2023-01-12 22:12:47 -0300

    torque needs testing for qsub and qdel

--- app/adapters/schedulerrepository.py
+++ app/adapters/schedulerrepository.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Dict, List, Union
 
@@ -349,8 +349,302 @@ class SGESchedulerRepository(AbstractSchedulerRepository):
             return Job(jobId=jobId, clusterId=Settings.clusterId)
 
 
+class TorqueSchedulerRepository(AbstractSchedulerRepository):
+    """"""
+
+    STATUS_MAPPING: Dict[str, JobStatus] = {
+        "Q": JobStatus.START_REQUESTED,
+        "W": JobStatus.START_REQUESTED,
+        "T": JobStatus.STARTING,
+        "R": JobStatus.RUNNING,
+        "H": JobStatus.STOP_REQUESTED,
+        "E": JobStatus.STOPPING,
+        "C": JobStatus.STOPPED,
+    }
+
+    @staticmethod
+    def __parse_to_datetime(time_str: str) -> timedelta:
+        hour, minute, second = time_str.split(":")
+        return timedelta(
+            hours=int(hour), minutes=int(minute), seconds=int(second)
+        )
+
+    @staticmethod
+    async def list_jobs() -> Union[List[Job], HTTPResponse]:
+        def __parse_list_jobs(content: str) -> List[Job]:
+            lines_to_skip = 5
+            lines = content.split("\n")
+            jobs: List[Job] = []
+            if len(lines) < 3:
+                return jobs
+            for line in lines[lines_to_skip:]:
+                if len(line) < 5:
+                    continue
+                status = TorqueSchedulerRepository.STATUS_MAPPING.get(
+                    line[102], JobStatus.UNKNOWN
+                )
+                startTime = (
+                    datetime.now()
+                    - TorqueSchedulerRepository.__parse_to_datetime(
+                        line[104:114].strip()
+                    )
+                )
+                jobId = line[:23].split(".")[0]
+                name = line[45:62].strip()
+                reservedSlots = int(line[75:82].strip())
+                if not all([jobId, name, reservedSlots]):
+                    continue
+                jobs.append(
+                    Job(
+                        jobId=str(jobId),
+                        name=str(name),
+                        status=status,
+                        startTime=startTime,
+                        lastStatusUpdateTime=datetime.now(),
+                        clusterId=Settings.clusterId,
+                        reservedSlots=int(reservedSlots),
+                    )
+                )
+            return jobs
+
+        cod, ans = await run_terminal_retry(["qstat -a"])
+        if cod != 0:
+            return HTTPResponse(code=500, detail=f"error running qstat: {ans}")
+        else:
+            return __parse_list_jobs(ans)
+
+    @staticmethod
+    async def get_job(jobId: str) -> Union[Job, HTTPResponse]:
+        def __parse_get_job(content: str) -> Union[Job, HTTPResponse]:
+            try:
+                lines = content.split("\n")
+                resourcelines = []
+                status = None
+                name = None
+                startTime = None
+                workingDirectory = None
+                reservedSlots = None
+                scriptFile = None
+                args = []
+                resourceUsage = None
+                for line in lines:
+                    if "Job_Name" in line:
+                        name = line.split("=")[1].strip()
+                    elif "job_state" in line:
+                        status = TorqueSchedulerRepository.STATUS_MAPPING.get(
+                            line.split("=")[1].strip(), JobStatus.UNKNOWN
+                        )
+                    elif "ctime" in line:
+                        startTime = datetime.strptime(
+                            line.split("=")[1].strip(), "%a %b %d %H:%M:%S %Y"
+                        )
+                    elif "Output_Path" in line:
+                        outfile = line.split("=")[1].strip().split(":")[1]
+                        workingDirectory = str(Path(outfile).parent)
+                    elif "Resource_List.nodes" in line:
+                        nodedata = line.split(" = ")[1].strip()
+                        procdata = nodedata.split(":ppn=")
+                        reservedSlots = int(procdata[0]) * int(procdata[1])
+                    elif "submit_args" in line:
+                        args = line.split("=")[1].strip().split(" ")
+                    elif "resources_used" in line:
+                        resourcelines.append(line)
+            except Exception:
+                return HTTPResponse(
+                    code=500, detail="error parsing qstat response"
+                )
+
+            # converts total memory from kB to GB
+            B_TO_GB = 1073741824
+            KB_TO_GB = 1048576
+            cpuSeconds = None
+            instantTotalMemory = None
+            maxTotalMemory = None
+            for line in resourcelines:
+                if ".cput" in line:
+                    cpuSeconds = TorqueSchedulerRepository.__parse_to_datetime(
+                        line.split("=")[1].strip()
+                    ).total_seconds()
+                elif ".mem" in line:
+                    instantTotalMemory = (
+                        float(line.split("=")[1].strip().split("kb")[0])
+                        / KB_TO_GB
+                    )
+                elif ".vmem" in line:
+                    maxTotalMemory = (
+                        float(line.split("=")[1].strip().split("kb")[0])
+                        / KB_TO_GB
+                    )
+            usage = (
+                ResourceUsage(
+                    cpuSeconds=cpuSeconds,
+                    memoryCpuSeconds=instantTotalMemory * cpuSeconds,
+                    instantTotalMemory=instantTotalMemory,
+                    maxTotalMemory=maxTotalMemory,
+                    processIO=0.0,
+                    processIOWaiting=0.0,
+                    timeInstant=datetime.now(),
+                )
+                if all([cpuSeconds, instantTotalMemory, maxTotalMemory])
+                else None
+            )
+            return Job(
+                jobId=str(jobId),
+                status=status,
+                name=str(name),
+                startTime=startTime,
+                lastStatusUpdateTime=datetime.now(),
+                clusterId=Settings.clusterId,
+                workingDirectory=str(workingDirectory),
+                reservedSlots=int(reservedSlots),
+                scriptFile=str(scriptFile),
+                args=args,
+                resourceUsage=usage,
+            )
+
+        cod, ans = await run_terminal_retry([f"qstat -f {jobId}"])
+        if cod != 0:
+            return HTTPResponse(
+                code=500, detail=f"error running qstat command: {ans}"
+            )
+        else:
+            detailedJob = __parse_get_job(ans)
+            if isinstance(detailedJob, HTTPResponse):
+                return HTTPResponse(
+                    code=500, detail="error parsing qstat -j result"
+                )
+            else:
+                return detailedJob
+
+    @staticmethod
+    async def get_finished_job(jobId: str) -> Union[Job, HTTPResponse]:
+        def __parse_get_job(content: str) -> Union[Job, HTTPResponse]:
+
+            # Iterates for getting info in the nodes
+            startTimeStr = "Job Run"
+            resourcesStr = "Exit_status="
+            timeStr = "resources_used.walltime="
+            cpuStr = "resources_used.cput="
+            memStr = "resources_used.mem="
+            maxVmemStr = "resources_used.vmem="
+            name = ""
+            startTime = None
+            endTime = None
+            slots = 0
+            cpu = 0.0
+            mem = 0.0
+            io = 0.0
+            iow = 0.0
+            maxvmem = 0.0
+            unitMultipliers = {
+                "kb": 1048576.0,
+                "M": 1024.0,
+                "G": 1.0,
+            }
+            for line in content.split("\n"):
+                print(line)
+                if startTimeStr in line:
+                    startTime = datetime.strptime(
+                        line[:19].strip(), "%m/%d/%Y %H:%M:%S"
+                    )
+                elif resourcesStr in line:
+                    # Filtra os trechos de cada informação
+                    cpu = float(line.split(cpuStr)[1].split(" ")[0])
+                    memdata = line.split(memStr)[1].split(" ")[0]
+                    mem = float(memdata[:-2]) / unitMultipliers[memdata[-2:]]
+                    vmemdata = line.split(maxVmemStr)[1].split(" ")[0]
+                    maxvmem = (
+                        float(vmemdata[:-2]) / unitMultipliers[vmemdata[-2:]]
+                    )
+                    runtime = TorqueSchedulerRepository.__parse_to_datetime(
+                        line.split(timeStr)[1].split(" ")[0]
+                    )
+                    endTime = startTime + runtime
+            if not all([startTime, endTime]):
+                return HTTPResponse(
+                    code=500,
+                    detail=f"error parsing tracejob response: {content}",
+                )
+
+            usage = ResourceUsage(
+                cpuSeconds=cpu,
+                memoryCpuSeconds=mem * cpu,
+                instantTotalMemory=mem,
+                maxTotalMemory=maxvmem,
+                processIO=io,
+                processIOWaiting=iow,
+                timeInstant=endTime,
+            )
+
+            return Job(
+                jobId=jobId,
+                status=JobStatus.STOPPED,
+                name=str(name),
+                startTime=startTime,
+                lastStatusUpdateTime=endTime,
+                endTime=endTime,
+                clusterId=Settings.clusterId,
+                reservedSlots=int(slots),
+                resourceUsage=usage,
+            )
+
+        cod, ans = await run_terminal_retry([f"tracejob {jobId}"])
+        if cod != 0:
+            return HTTPResponse(code=404, detail=f"job {jobId} not found")
+        else:
+            detailedJob = __parse_get_job(ans)
+            return detailedJob
+
+    @staticmethod
+    async def submit_job(job: Job) -> Union[Job, HTTPResponse]:
+        def __parse_submit_ans(content: str):
+            job.jobId = content.split(".")[0].strip()
+
+        if not job.name:
+            job.name = Path(job.workingDirectory).parts[-1]
+        if not job.workingDirectory:
+            return HTTPResponse(
+                code=400, detail="workingDirectory is mandatory"
+            )
+        if not job.reservedSlots:
+            return HTTPResponse(code=400, detail="reservedSlots is mandatory")
+        if not job.scriptFile:
+            return HTTPResponse(code=400, detail="scriptFile is mandatory")
+        command = [
+            "qsub",
+            job.scriptFile,
+            "-N",
+            job.name,
+            "-l",
+            f"nodes={job.reservedSlots}",
+            *(job.args),
+        ]
+        with set_directory(job.workingDirectory):
+            cod, ans = await run_terminal_retry(command)
+        if cod != 0:
+            return HTTPResponse(
+                code=500, detail=f"error running qsub command: {ans}"
+            )
+        else:
+            __parse_submit_ans(ans)
+            return job
+
+    @staticmethod
+    async def stop_job(jobId: str) -> Union[Job, HTTPResponse]:
+        # TODO - validate required fields
+        command = ["qdel", jobId]
+        cod, ans = await run_terminal_retry(command)
+        if cod != 0:
+            return HTTPResponse(
+                code=500, detail=f"error running qdel command: {ans}"
+            )
+        else:
+            return Job(jobId=jobId, clusterId=Settings.clusterId)
+
+
 SUPPORTED_SCHEDULERS: Dict[str, AbstractSchedulerRepository] = {
-    "SGE": SGESchedulerRepository
+    "SGE": SGESchedulerRepository,
+    "TORQUE": TorqueSchedulerRepository,
 }
 DEFAULT = SGESchedulerRepository
 
